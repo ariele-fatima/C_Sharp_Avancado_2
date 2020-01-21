ff6658cf3e4d333825eb52bfca480ab25bd7d454
ariele-fatima <ariele_afd@yahoo.com.br>
2020-01-20 21:22:42 -0300

    AB#35 NHibernate, criando a infraestrutura necessária

--- TreinaWeb.CSharpAvancado.CadastroPessoas/TreinaWeb.CadastroPessoas.Repositorio/PessoaRepositorio.cs
+++ TreinaWeb.CSharpAvancado.CadastroPessoas/TreinaWeb.CadastroPessoas.Repositorio/PessoaRepositorio.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using CadastroPessoas.Persistencia.NHibernate.Maps;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Cfg.MappingSchema;
+using NHibernate.Mapping.ByCode;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +15,25 @@ namespace TreinaWeb.CadastroPessoas.Repositorio
 {
     public class PessoaRepositorio : IRepositorio<Pessoa>
     {
+        private ISessionFactory _sessionFactory;
+
+        public PessoaRepositorio()
+        {
+            Configuration config = new Configuration();
+            config.Configure(); 
+            config.AddAssembly(typeof(Pessoa).Assembly);
+            HbmMapping mapping = CreateMappings();
+            config.AddDeserializedMapping(mapping, null);
+            _sessionFactory = config.BuildSessionFactory();
+        }
+
+        private HbmMapping CreateMappings()
+        {
+            ModelMapper mapper = new ModelMapper();
+            mapper.AddMapping(typeof(PessoaMap));
+            return mapper.CompileMappingForAllExplicitlyAddedEntities();
+        }
+
         public List<Pessoa> SelecionarTodos()
         {
             CadastroPessoasDbContext contexto = new CadastroPessoasDbContext();

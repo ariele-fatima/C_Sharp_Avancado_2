8c1d446aff5d2da64c53af134b962fb86668f03a
ariele-fatima <ariele_afd@yahoo.com.br>
2020-02-02 19:03:27 -0300

    AB#36 - Criando uma camada de apresentcao com WPF

---
+++ TreinaWeb.CSharpAvancado.CadastroPessoas/TreinaWeb.CSharpAvancado.CadastroPessoas.WPF/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TreinaWeb.CadastroPessoas.Dominio;
+using TreinaWeb.CadastroPessoas.Repositorio;
+
+namespace TreinaWeb.CSharpAvancado.CadastroPessoas.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CarregarDataGrid()
+        {
+            IRepositorio<Pessoa> repositorioPessoas = new PessoaRepositorio();
+            List<Pessoa> pessoas = repositorioPessoas.SelecionarTodos();
+            dgrPessoas.ItemsSource = pessoas;
+        }
+
+        private void WdwMain_Loaded(object sender, RoutedEventArgs e)
+        {
+            CarregarDataGrid();
+        }
+
+        private void btnCadastrarPessoa_Click(object sender, RoutedEventArgs e)
+        {
+            WndCadastrarPessoa wndCadastrarPessoa = new WndCadastrarPessoa();
+            wndCadastrarPessoa.ShowDialog();
+            CarregarDataGrid();
+        }
+    }
+}
---
+++ TreinaWeb.CSharpAvancado.CadastroPessoas/TreinaWeb.CSharpAvancado.CadastroPessoas.WPF/WndCadastrarPessoa.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TreinaWeb.CadastroPessoas.Dominio;
+using TreinaWeb.CadastroPessoas.Repositorio;
+
+namespace TreinaWeb.CSharpAvancado.CadastroPessoas.WPF
+{
+    /// <summary>
+    /// Interaction logic for WndCadastrarPessoa.xaml
+    /// </summary>
+    public partial class WndCadastrarPessoa : Window
+    {
+        public WndCadastrarPessoa()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalvarPessoa_Click(object sender, RoutedEventArgs e)
+        {
+            IRepositorio<Pessoa> repositorioPessoas = new PessoaRepositorio();
+            Pessoa pessoa = new Pessoa
+            {
+                Nome = txbNomePessoa.Text,
+                Idade = Convert.ToInt32(txbIdadePessoa.Text),
+                Endereco = txbEnderecoPessoa.Text
+            };
+            repositorioPessoas.Adicionar(pessoa);
+            Close();
+        }
+    }
+}
